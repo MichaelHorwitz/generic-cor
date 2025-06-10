4e01f281a84ff4ba61357d07fc244fed790b70ce
Michael Horwitz <michael@horwitz.co.za>
2025-06-10 12:26:25 +0200

    formatting

--- Handler.hpp
+++ Handler.hpp
@@ -1,24 +1,29 @@
 #pragma once
 #include <memory>
 
-
 class Handler {
-typedef std::unique_ptr<Handler> hdlrPtr; // NOLINT
+    typedef std::unique_ptr<Handler> hdlrPtr; // NOLINT
     hdlrPtr successor;
-public:
 
-virtual ~Handler() = default;
-explicit Handler() { successor = nullptr;};
-    explicit Handler(hdlrPtr s) : successor(std::move(s)) { }
+public:
+    virtual ~Handler() = default;
+
+    explicit Handler() { successor = nullptr; };
+
+    explicit Handler(hdlrPtr s) : successor(std::move(s)) {
+    }
+
     virtual void handleRequest(int amount) { // NOLINT
         if (successor) {
             successor->handleRequest(amount);
         }
     };
+
     void setSuccessor(hdlrPtr s) {
         this->successor = std::move(s);
     };
-    [[nodiscard]] Handler* getSuccessor() const {
+
+    [[nodiscard]] Handler *getSuccessor() const {
         return successor.get();
     }
 };
--- TwentyHandler.hpp
+++ TwentyHandler.hpp
@@ -2,13 +2,17 @@
 #include <iostream>
 #include "Handler.hpp"
 
-class TwentyHandler : public Handler{
+class TwentyHandler : public Handler {
     int numberOfNotes = 20;
+
 public:
-    TwentyHandler(){ Handler();}
+    TwentyHandler() { Handler(); }
+
     explicit TwentyHandler(std::unique_ptr<Handler> next)
-    : Handler(std::move(next)) {}
-    void handleRequest(int amount) {
+        : Handler(std::move(next)) {
+    }
+
+    void handleRequest(int amount) override {
         while (amount >= 20) {
             amount -= 20;
             std::cout << "Gave R20 remaining amount: " << amount << std::endl;
